48803851832477d79203585ef1c301a4a069b047
Connor Ward <c.ward20@imperial.ac.uk>
2023-06-06 08:52:22 +0100

    All integration tests passing

--- tests/test_loopexpr.py
+++ tests/test_loopexpr.py
@@ -1010,12 +1010,15 @@ def test_different_axis_orderings_do_not_change_packing_order():
     #     name="copy",
     #     lang_version=(2018, 2),
     # )
+    m0, m1, m2 = 5, 2, 2
+    npoints = m0 * m1 * m2
+
     code = lp.make_kernel(
-        "{ [i]: 0 <= i < 4 }",
+        f"{{ [i]: 0 <= i < {m1*m2} }}",
         "y[i] = x[i]",
         [
-            lp.GlobalArg("x", np.float64, (4,), is_input=True, is_output=False),
-            lp.GlobalArg("y", np.float64, (4,), is_input=False, is_output=True),
+            lp.GlobalArg("x", ScalarType, (m1 * m2,), is_input=True, is_output=False),
+            lp.GlobalArg("y", ScalarType, (m1 * m2,), is_input=False, is_output=True),
         ],
         target=lp.CTarget(),
         name="copy",
@@ -1023,29 +1026,29 @@ def test_different_axis_orderings_do_not_change_packing_order():
     )
     copy_kernel = LoopyKernel(code, [READ, WRITE])
 
-    axis0 = Axis([AxisComponent(2)], "ax0")
-    axis1 = Axis([AxisComponent(2)], "ax1")
-    axis2 = Axis([AxisComponent(2)], "ax2")
+    axis0 = Axis(m0, "ax0")
+    axis1 = Axis(m1, "ax1")
+    axis2 = Axis(m2, "ax2")
 
     axes0 = AxisTree(axis0, {axis0.id: [axis1], axis1.id: [axis2]})
     axes1 = AxisTree(axis0, {axis0.id: [axis2], axis2.id: [axis1]})
 
-    dat0_0 = MultiArray(
-        axes0, name="dat0_0", data=np.arange(2 * 2 * 2, dtype=np.float64)
-    )
-    dat0_1 = MultiArray(
-        axes1, name="dat0_1", data=np.array([0, 2, 1, 3, 4, 6, 5, 7], dtype=np.float64)
-    )
-    dat1 = MultiArray(axes0, name="dat1", data=np.zeros(2 * 2 * 2, dtype=np.float64))
+    data0 = np.arange(npoints, dtype=ScalarType).reshape((m0, m1, m2))
+    data1 = data0.swapaxes(1, 2)
 
-    p = IndexTree(Index([Range(("ax0", 0), 2)]))
-    q = IndexTree(
-        p.root,
-        {
-            p.root.id: [Index([Range(("ax1", 0), 2)], id="idx_id0")],
-            "idx_id0": [Index([Range(("ax2", 0), 2)])],
-        },
+    dat0_0 = MultiArray(
+        axes0,
+        name="dat0_0",
+        data=data0.flatten(),
     )
+    dat0_1 = MultiArray(axes1, name="dat0_1", data=data1.flatten())
+    dat1 = MultiArray(axes0, name="dat1", data=np.zeros(npoints, dtype=ScalarType))
+
+    p = IndexTree(Index(Range("ax0", m0)))
+
+    q = p.copy()
+    q = q.put_node(Index(Range("ax1", m1)), q.leaf)
+    q = q.put_node(Index(Range("ax2", m2)), q.leaf)
 
     do_loop(p, copy_kernel(dat0_0[q], dat1[q]))
     assert np.allclose(dat1.data, dat0_0.data)
