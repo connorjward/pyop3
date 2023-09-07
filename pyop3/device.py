c7adf24adb78b912576af7cf3d2ac20c246afe5e
Connor Ward <c.ward20@imperial.ac.uk>
2023-09-07 14:39:03 +0100

    WIP

---
+++ pyop3/device.py
@@ -0,0 +1,39 @@
+import abc
+import contextlib
+
+
+class OffloadingDevice(abc.ABC):
+    pass
+
+
+class CPUDevice(OffloadingDevice):
+    pass
+
+
+class GPUDevice(OffloadingDevice, abc.ABC):
+    def __init__(self, num_threads=32):
+        self.num_threads = num_threads
+
+
+class CUDADevice(GPUDevice):
+    pass
+
+
+class OpenCLDevice(GPUDevice):
+    pass
+
+
+offloading_device = OffloadingDevice.CPU
+
+
+@contextlib.contextmanager
+def offloading(device: OffloadingDevice):
+    global offloading_device
+
+    orig_offloading_device = offloading_device
+    if not isinstance(orig_offloading_device, OffloadingDevice.CPU):
+        raise NotImplementedError("Not sure what to do when offloading from not a CPU")
+
+    offloading_device = device
+    yield
+    offloading_device = orig_offloading_device
