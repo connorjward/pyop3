492427a5396368b0bf9e1983a038f70ffe96c528
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2023-09-13 01:02:55 +0100

    Ooops

--- scripts/requirements.py
+++ scripts/requirements.py
@@ -5,7 +5,7 @@ import toml
 pyproject = toml.load("pyproject.toml")
 if "build" in sys.argv:
     packages = pyproject["build-system"]["requires"]
-elif "requirements" in sys.argv:
+elif "run" in sys.argv:
     packages = pyproject["project"]["dependencies"]
 
 print("\n".join([pkg for pkg in packages if pkg != "petsc4py"]))
